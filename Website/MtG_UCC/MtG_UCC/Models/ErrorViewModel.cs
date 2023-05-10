e326f085bcdd2a18dbc119d15bc5167fd525faa1
CaineSmith <cls56@students.ptcollege.edu>
2023-05-10 12:50:51 -0400

    Initial Website Upload
    This upload represents the first step towards the ultimate goal of finishing this website and project. At present, email verification has been installed using SendGrid and work on Captcha implementation is ongoing. Tests have been excluded from this upload as they are in a separate project and are Selenium based; these tests are still in theoretical stage, as they have not been run against the project as of yet, due to a lack of a test-ready product.

---
+++ Website/MtG_UCC/MtG_UCC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MtG_UCC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Website/MtG_UCC/MtG_UCC/Services/SendGrid/SendGridOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace MtG_UCC.Services.SendGrid
+{
+    public class SendGridOptions
+    {
+        public const string SendGrid = "SendGrid";
+        public string ApiKey { get; set; }
+        public string FromEmail { get; set; }
+        public string FromName { get; set; }
+        public string RegisterTemplateID { get; set; }
+    }
+}
